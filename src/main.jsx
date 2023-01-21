23e561a5af36f96d5b6c2056f7e27365b1accd76
Oscar <oscarantonio.soto1998@gmail.com>
2023-01-21 01:05:29 -0300

    Fix: add AOS animation

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Portafolio } from './Portafolio'
-
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
 import './index.css'
 
+AOS.init();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Portafolio />
